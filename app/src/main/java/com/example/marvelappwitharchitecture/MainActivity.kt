0adf5dcb4ea5ec094286d4b9764a6d5ee002b9cd
Angel Lopera Casas <107440117+alopera25@users.noreply.github.com>
2024-09-02 12:52:39 +0200

    Add TopAppBar using a Scaffold

--- app/src/main/java/com/example/marvelappwitharchitecture/MainActivity.kt
+++ app/src/main/java/com/example/marvelappwitharchitecture/MainActivity.kt
@@ -15,32 +15,53 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.marvelappwitharchitecture.ui.theme.MarvelAppWithArchitectureTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             MarvelAppWithArchitectureTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    LazyVerticalGrid(
-                        columns = GridCells.Adaptive(120.dp),
-                        contentPadding = PaddingValues(4.dp),
-                        horizontalArrangement = Arrangement.spacedBy(4.dp),
-                        verticalArrangement = Arrangement.spacedBy(4.dp)
-                    ) {
-                        items(characters, key = { it.id }) {
-                            CharacterItem(character = it)
+                    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = { Text(text = stringResource(id = R.string.app_name)) },
+                                scrollBehavior = scrollBehavior
+                            )
+                        },
+                        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+                    ) { padding ->
+                        LazyVerticalGrid(
+                            columns = GridCells.Adaptive(120.dp),
+                            contentPadding = PaddingValues(4.dp),
+                            horizontalArrangement = Arrangement.spacedBy(4.dp),
+                            verticalArrangement = Arrangement.spacedBy(4.dp),
+                            modifier = Modifier.padding(padding)
+                        ) {
+                            items(characters, key = { it.id }) {
+                                CharacterItem(character = it)
+                            }
                         }
                     }
                 }
